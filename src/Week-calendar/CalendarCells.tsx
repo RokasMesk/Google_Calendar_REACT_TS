859dbde1206b024f59ce76c21784fa594945c3a1
RokasMesk <rokasme@wix.com>
2024-07-23 11:36:36 +0300

    removed console.log

--- src/Week-calendar/CalendarCells.tsx
+++ src/Week-calendar/CalendarCells.tsx
@@ -3,7 +3,6 @@ import styles from './week-calendar.module.css';
 const CalendarCells = () => (
   <div className={styles.calendarCells} id="calendarCells">
     {new Array(7 * 19).fill(undefined).map((_, i) => {
-      console.log(i);
       return <div className={styles.cell} key={i}></div>;
     })}
   </div>
